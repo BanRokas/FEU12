df8bce62001ee889cb810a18d5782e31fb5e1fee
BanRokas <rokas.banaitis@gmail.com>
2024-01-19 10:58:45 +0200

    22 paskaita - Masyvų metodai

--- JavaScript/22 paskaita - 01.19 - Masyvų Metodai ir Bevardės funkcijos/main.js
+++ JavaScript/22 paskaita - 01.19 - Masyvų Metodai ir Bevardės funkcijos/main.js
@@ -59,13 +59,24 @@ console.log(objektoRaktai);
   Metodai:
     prototype.at(index) - grąžina masyvo elementą esantį nurodytame indekse
     prototype.concat(arr1, arr2, ...) - grąžina masyvų junginį kaip naują masyvą
-    prototype
-    prototype
-    prototype
-    prototype
-    prototype
-    prototype
-    prototype
-    prototype
-
+    prototype.fill(value, start, end) - užpildo masyvą nurodytomis reikšmėmis (value) nuo nurodytos pozicijos (start) iki nurodytos pozicijos (end). MODIFIKUOJA originalų masyvą ir grąžina modifikuotą.
+    prototype.includes(value) - tikrina ar masyve yra nurodyta reikšmė (value). Grąžina true arba false.
+    prototype.indexOf(value, from) - grąžina ieškomos reikšmės (value) indeksą masyve nuo nurodytos pozicijos (from). Jeigu neranda - grąžina -1;
+    prototype.lastIndexOf(value, from) - grąžina ieškomos reikšmės (value) indeksą masyve nuo nurodytos pozicijos (from) nuo masyvo galo. Jeigu neranda - grąžina -1;
+    .isArray(value) - tikrina ar value (reikšmė) yra masyvas.
+    prototype.join(separator) - grąžina apjungtas masyvo reikšmės į string'ą atskirtas nurodytu skirtuku (separator). NEmodifikuoja originalaus masyvo.
+    .of(value1, value2, value3, ...) - sugeneruoja masyvą su nurodytomos reikšmėmis
+    prototype.pop()
+    prototype.push(value)
+    prototype.shift()
+    prototype.unshift(value)
+    prototype.reverse() - apsuka masyvą iš kito galo. MODIFIKUOJA originalų masyvą
+    prototype.toReversed() - grąžina apsuktą masyvą iš kito galo. NEmodifikuoja originalaus masyvo.
+    prototype.slice(from, to)
+    prototype.sort(compareFn)
+    prototype.toSorted(compareFn) - grąžina naują surikiuotą masyvą. NEmodifikuoja originalaus masyvo.
+    prototype.splice(start, deleteCount, value1, value2, ...)
+    prototype.toSpliced(start, deleteCount, value1, value2, ...) - grąžina naują pakeistą masyvą. NEmodifikuoja originalaus masyvo.
+    prototype.with(index, value) - grąžina naują masyvą, kuriame nurodytame indekse reikšmė yra pakeista į nurodytą value (reikšmę). NEmodifikuoja originalaus masyvo.
+      prototype[index] = value; - originalus masyvas yra MODIFIKUOJAMAS.
 */
