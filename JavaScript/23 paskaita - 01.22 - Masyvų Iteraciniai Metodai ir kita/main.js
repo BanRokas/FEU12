bb0b80e16fe9a6c0686fc5b0499b56d3fd656b45
BanRokas <rokas.banaitis@gmail.com>
2024-01-22 12:00:03 +0200

    23 paskaita -  destr objektai ir parametrai

--- JavaScript/23 paskaita - 01.22 - Masyvų Iteraciniai Metodai ir kita/main.js
+++ JavaScript/23 paskaita - 01.22 - Masyvų Iteraciniai Metodai ir kita/main.js
@@ -91,16 +91,17 @@ console.groupCollapsed('masyvų des')
     return pilnasVardas.split(' ');
   }
   // let atsPvzDesMas = pvzDesMas('Vardenis Pavardenis');
-  // let vardas = atsPvzDesMas[0];
-  // let pavarde = atsPvzDesMas[1];
-  let [vardas, pavarde] = pvzDesMas('Vardenis Pavardenis');
-  console.log(vardas, pavarde);
+  // let vardasF = atsPvzDesMas[0];
+  // let pavardeF = atsPvzDesMas[1];
+  let [vardasF, pavardeF] = pvzDesMas('Vardenis Pavardenis');
+  console.log(vardasF, pavardeF);
 console.groupEnd();
-console.group('objektų des');
+console.groupCollapsed('objektų des');
   let objektasDes = {
     vardas: "Petras",
     amzius: 55,
     pavarges: true,
+    batuDydis: 45,
     vairuotojoTeises: undefined,
     gyvenamojiVieta: {
       miestas: 'Vilnius',
@@ -108,5 +109,47 @@ console.group('objektų des');
     },
     pomegiai: ['kartingai', 'baidarės', 'žygiavimas']
   };
-  
+  // be destr
+  let objVardas = objektasDes.vardas;
+  let objSalis = objektasDes.gyvenamojiVieta.salis;
+  let objPomegis = objektasDes.pomegiai[2];
+  let objVair = objektasDes.vairuotojoTeises !== undefined ? objektasDes.vairuotojoTeises : false;
+  console.log(objVardas, objSalis, objPomegis, objVair);
+  // let objLike = {
+  //   amzius: objektasDes.amzius,
+  //
+  // }
+  // su destr
+  let { vardas, amzius:metai, gyvenamojiVieta:{salis}, vairuotojoTeises=false, pomegiai:{2:pomegis}, ...restObj } = objektasDes;
+  console.log(vardas, metai, salis, vairuotojoTeises, pomegis, restObj);
+  /*
+    destruktūrizuojant objekta kreipiesi į raktinius žodžius
+    raktinius žodžius gali pervadinti - raktZod:kitasPav
+    gali kreiptis į raktą esanti objekte - objViduje:{raktas}
+    gali nurodyti default reikšmę jei originalioji yra undefined naudojant lygybę
+    galima kreiptis į elementą masyve per indeksą, bet privalu nurodyti naują pavadinimą - masViduje:{index:sugalvotasPav}
+    galima susirinkti likusias NEPALIESTAS objekto reikšmes naudojant spread operatorių ...
+  */
+console.groupEnd();
+console.group('parametrų destr'); // nieko naujo, tas pats kas ir objektų ar masyvų destr tik, kad parametrų aprašymo vietoje
+  function spreadPvz(tekstas, ...skaiciai){
+    console.log(tekstas);
+    console.log(skaiciai);
+  }
+  spreadPvz('labas rytas', 4,6,5,7,8,9,45,54,564,6,516,1,81,65,16,16,1);
+  // function objKaipParDes(kazka){
+  //   console.log(kazka);
+  //   console.log(`${kazka.vardas} yra ${kazka.amzius} metų amžiaus.`);
+  // }
+  // function objKaipParDes(kazka){
+  //   console.log(kazka);
+  //   let vardas = kazka.vardas;
+  //   let amzius = kazka.amzius;
+  //   console.log(`${vardas} yra ${amzius} metų amžiaus.`);
+  // }
+  function objKaipParDes({ vardas, amzius, ...rest }){
+    console.log(vardas, amzius, rest);
+    console.log(`${vardas} yra ${amzius} metų amžiaus.`);
+  }
+  objKaipParDes(objektasDes);
 console.groupEnd();
