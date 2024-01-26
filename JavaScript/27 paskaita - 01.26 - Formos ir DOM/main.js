dd47f9b79c1a4d9d07c761661546a841d6b2105a
BanRokas <rokas.banaitis@gmail.com>
2024-01-26 12:04:52 +0200

    27 paskaita - Formos ir DOM harder done

--- JavaScript/27 paskaita - 01.26 - Formos ir DOM/main.js
+++ JavaScript/27 paskaita - 01.26 - Formos ir DOM/main.js
@@ -26,7 +26,7 @@ document
   .addEventListener('submit', e => {
     e.preventDefault();
     const el = e.target.elements;
-    const info = {
+    const data = {
       vardas: el.asmuo.value,
       kontaktas: el.kontaktas.value,
       modelis: el.modelis.value,
@@ -37,5 +37,52 @@ document
       lokacija: el.lokacija.value,
       dataLaikas: el.dataLaikas.value
     };
-    console.log(info);
+    console.log(data);
+
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('telCard');
+
+    const lokacija = document.createElement('h2');
+    const lokacijaText = document.createTextNode(data.lokacija);
+    lokacija.appendChild(lokacijaText);
+
+    const vardas = document.createElement('p');
+    const vardasText = document.createTextNode(`${data.vardas} ir ${data.kontaktas}`);
+    vardas.appendChild(vardasText);
+
+    const atnesti = document.createElement('p');
+    const atnestiText = document.createTextNode(`Data: ${data.dataLaikas.slice(0,10)} Laikas:${data.dataLaikas.slice(11)}`);
+    atnesti.appendChild(atnestiText);
+
+    const modelis = document.createElement('h3');
+    const modelisText = document.createTextNode(data.modelis);
+    modelis.appendChild(modelisText);
+
+    const aprasymas = document.createElement('p');
+    const aprasymasText = document.createTextNode(data.gedimas);
+    aprasymas.appendChild(aprasymasText);
+
+    const paveiksliukas = document.createElement('img');
+    paveiksliukas.setAttribute('src', data.foto);
+    paveiksliukas.setAttribute('alt', 'sugedusio telefono nuotrauka');
+
+    const draudimasIrGarantija = document.createElement('p');
+    const draudimas = document.createElement('span');
+    const draudimasText = document.createTextNode('Draudimas');
+    draudimas.appendChild(draudimasText);
+    if(data.draudimas){
+      draudimas.classList.add('greenText');
+    } else {
+      draudimas.classList.add('redText');
+    }
+    const garantija = document.createElement('span');
+    const garantijaText = document.createTextNode('Garantija');
+    garantija.appendChild(garantijaText);
+    // data.garantinis ? garantija.classList.add('greenText') : garantija.classList.add('redText');
+    garantija.classList.add(data.garantinis ? 'greenText' : 'redText');
+    draudimasIrGarantija.append(draudimas, garantija);
+
+    cardDiv.append(lokacija, vardas, atnesti, modelis, aprasymas, paveiksliukas, draudimasIrGarantija);
+
+    document.querySelector('#phoneContainer').appendChild(cardDiv);
   });
