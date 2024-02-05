eaca2c54893cf0e37fa65715740feaa97d488533
BanRokas <rokas.banaitis@gmail.com>
2024-02-05 11:48:55 +0200

    33 paskaita - Fetch and do stuff with ISS

--- JavaScript/33 paskaita - 02.05 - Fetch/main.js
+++ JavaScript/33 paskaita - 02.05 - Fetch/main.js
@@ -27,3 +27,23 @@ fetch('https://randomuser.me/api')
   .finally(() => { console.log("Baigėsi fetch'as") });
 
 console.log('po');
+
+const fetchISS = () => {
+  fetch(`https://api.wheretheiss.at/v1/satellites/25544`)
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      document.querySelector('#lat').textContent = data.latitude.toFixed(3);
+      document.querySelector('#lon').textContent = data.longitude.toFixed(3);
+    });
+}
+
+// setInterval(fetchISS, 1000);
+
+// window.addEventListener('load', fetchISS);
+// window.onload = () => fetchISS();
+fetchISS();
+
+document
+  .querySelector('#updateISS')
+  .addEventListener('click', fetchISS);
