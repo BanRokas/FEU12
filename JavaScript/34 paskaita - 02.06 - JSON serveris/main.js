cd5356d7a866c66bab2dfe595f3c84aa93a8ba0e
BanRokas <rokas.banaitis@gmail.com>
2024-02-06 12:03:01 +0200

    34 paskaita - metodai kode

--- JavaScript/34 paskaita - 02.06 - JSON serveris/main.js
+++ JavaScript/34 paskaita - 02.06 - JSON serveris/main.js
@@ -1,13 +1,90 @@
-// gauti visus filmus
+//          gauti visus filmus
 // fetch(`http://localhost:3000/filmai`)
 //   .then(res => res.json())
 //   .then(data => {
 //     console.log(data);
 //   })
 
-// gauti vieną specifinį filmą
+//          gauti vieną specifinį filmą
 // fetch(`http://localhost:3000/filmai/2`)
 //   .then(res => res.json())
 //   .then(data => {
 //     console.log(data)
 //   })
+
+//            papildyti vienu extra filmu
+// const filmas = {
+//   id: new Date()*1, // Math.random()
+//   pavadinimas: "The Intouchables",
+//   leidimoData: 2011,
+//   nuotrauka: "https://m.media-amazon.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_FMjpg_UX1000_.jpg"
+// };
+// fetch(`http://localhost:3000/filmai`, {
+//   method: "POST",
+//   headers:{
+//     "Content-Type":"application/json"
+//   },
+//   body: JSON.stringify(filmas)
+// })
+// nelabai naudinga \/
+  // .then(res => res.json())
+  // .then(data => console.log(data))
+
+//          papildyti daugeliu extra filmų
+// const filmai = [
+//   {
+//     id: new Date()*1, // Math.random()
+//     pavadinimas: "The Intouchables",
+//     leidimoData: 2011,
+//     nuotrauka: "https://m.media-amazon.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_FMjpg_UX1000_.jpg"
+//   },{
+//     id: new Date()*1, // Math.random()
+//     pavadinimas: "The Intouchables0",
+//     leidimoData: 2011,
+//     nuotrauka: "https://m.media-amazon.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_FMjpg_UX1000_.jpg"
+//   },{
+//     id: new Date()*1, // Math.random()
+//     pavadinimas: "The Intouchables1",
+//     leidimoData: 2011,
+//     nuotrauka: "https://m.media-amazon.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_FMjpg_UX1000_.jpg"
+//   },{
+//     id: new Date()*1, // Math.random()
+//     pavadinimas: "The Intouchables2",
+//     leidimoData: 2011,
+//     nuotrauka: "https://m.media-amazon.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_FMjpg_UX1000_.jpg"
+//   },{
+//     id: new Date()*1, // Math.random()
+//     pavadinimas: "The Intouchables3",
+//     leidimoData: 2011,
+//     nuotrauka: "https://m.media-amazon.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_FMjpg_UX1000_.jpg"
+//   }
+// ];
+// filmai.forEach(filmas => {
+//   fetch(`http://localhost:3000/filmai`, {
+//     method: "POST",
+//     headers:{
+//       "Content-Type":"application/json"
+//     },
+//     body: JSON.stringify(filmas)
+//   })
+// })
+
+//          trinti vieną specifinį filmą
+// fetch(`http://localhost:3000/filmai/1707212747422`, {
+//   method: "DELETE"
+// })
+// nelabai naudinga \/
+// .then(res => {
+//   console.log(res);
+//   return res.json();
+// }).then(data => {
+//   console.log(data);
+// })
+
+//        trinti daug filmų
+// const trintiMasyvas = ["1707212177779", "1707212213888", "1707212215441", "1707212747422", "1707212747422", "1707212747422"];
+// trintiMasyvas.forEach(trinamas => {
+//   fetch(`http://localhost:3000/filmai/${trinamas}`, {
+//     method: "DELETE"
+//   })
+// })
