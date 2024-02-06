07a1f142218f502fe5af8c474549cd183bf70289
BanRokas <rokas.banaitis@gmail.com>
2024-02-06 11:05:30 +0200

    34 paskaita - metodai kode

---
+++ JavaScript/34 paskaita - 02.06 - JSON serveris/main.js
@@ -0,0 +1,13 @@
+// gauti visus filmus
+// fetch(`http://localhost:3000/filmai`)
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data);
+//   })
+
+// gauti vieną specifinį filmą
+// fetch(`http://localhost:3000/filmai/2`)
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data)
+//   })
