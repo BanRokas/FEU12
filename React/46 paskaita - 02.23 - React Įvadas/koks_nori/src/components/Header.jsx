f0a1dde615b80767937286d7a452b8e07b816514
BanRokas <rokas.banaitis@gmail.com>
2024-02-23 11:19:31 +0200

    46 paskaita - Header

--- React/46 paskaita - 02.23 - React Įvadas/koks_nori/src/components/Header.jsx
+++ React/46 paskaita - 02.23 - React Įvadas/koks_nori/src/components/Header.jsx
@@ -1,8 +1,17 @@
 const Header = () => {
   return (
-    <>
-      <p>Headeris</p>
-    </>
+    <header>
+      <img src="https://quintagroup.com/cms/js/js-image/react.js-logo.png/@@images/a9bf22bd-373a-4fae-a900-c22fd12c87c7.png" alt="react logo" />
+      <nav>
+        <ul>
+          <li><a href="#">pirmas</a></li>
+          <li><a href="#">antras</a></li>
+          <li><a href="#">trečias</a></li>
+          <li><a href="#">ketvirtas</a></li>
+          <li><a href="#">penktas</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
  
