4088f38c69e5909a75c831588b362c76c9c341e3
BanRokas <rokas.banaitis@gmail.com>
2024-02-23 11:31:40 +0200

    46 paskaita - Hero

--- React/46 paskaita - 02.23 - React Įvadas/koks_nori/src/components/Hero.jsx
+++ React/46 paskaita - 02.23 - React Įvadas/koks_nori/src/components/Hero.jsx
@@ -1,8 +1,9 @@
 function Hero(){
   return (
-    <>
-      <p>Hero</p>
-    </>
+    <section id="hero">
+      <h1>React <br/> is <br /> POG</h1>
+      <img src="https://cdn-icons-png.flaticon.com/512/1183/1183672.png" alt="REACT" />
+    </section>
   );
 }
 
