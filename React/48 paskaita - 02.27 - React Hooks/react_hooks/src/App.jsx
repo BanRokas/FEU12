c5757ac3f3914c456c05735e9e67b53ff6e2bb3e
BanRokas <rokas.banaitis@gmail.com>
2024-02-27 09:12:10 +0200

    48 paskaita - useState intro

--- React/48 paskaita - 02.27 - React Hooks/react_hooks/src/App.jsx
+++ React/48 paskaita - 02.27 - React Hooks/react_hooks/src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
 
+  // let skaicius = 0;
+  let [skaicius, setSkaicius] = useState(0);
+  console.log(skaicius);
+  const mazintiSkaiciu = () => {
+    // skaicius--;
+    setSkaicius(skaicius-1);
+    // console.log(skaicius);
+  }
+  const didintiSkaiciu = () => {
+    // skaicius++;
+    setSkaicius(skaicius+1);
+    // console.log(skaicius);
+  }
+
   return (
     <>
-      
+      <p>Mūsų skaičius yra: {skaicius}</p>
+      <button onClick={mazintiSkaiciu}>Mažinti Skaičių</button>
+      <button onClick={() => didintiSkaiciu()}>Didinti Skaičių</button>
     </>
   );
 }
