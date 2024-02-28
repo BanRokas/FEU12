a6f4d78f6c715a8b9366932422c6ae8ae67022d9
BanRokas <rokas.banaitis@gmail.com>
2024-02-28 09:10:25 +0200

    49 paskaita - food variable

--- React/49 paskaita - 02.28 - useEffect/use_effect/src/App.jsx
+++ React/49 paskaita - 02.28 - useEffect/use_effect/src/App.jsx
@@ -1,6 +1,52 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [food, setFood] = useState([
+    {
+      id: 0,
+      pavadinimas: 'Kebabas',
+      kilmesSalis: 'Bizantija',
+      ragautas: true,
+      ingredientai: ['mėsa', 'padažas', 'daržovės', 'lavašas'],
+      kaina: {
+        nuo: 4.5,
+        iki: 8
+      }
+    },{
+      id: 1,
+      pavadinimas: 'Pica',
+      kilmesSalis: 'Italija',
+      ragautas: true,
+      ingredientai: ['picos padas', 'padažas', 'sūris', 'mėsa'],
+      kaina: {
+        nuo: 8,
+        iki: 16
+      }
+    },{
+      id: 3,
+      pavadinimas: 'Cepelinai',
+      kilmesSalis: 'Lietuva',
+      ragautas: true,
+      ingredientai: ['bulvė', 'mėsa', 'grietinė', 'spirgučiai'],
+      kaina: {
+        nuo: 1.5,
+        iki: 5
+      }
+    },{
+      id: 4,
+      pavadinimas: 'Sevičė',
+      kilmesSalis: 'Peru',
+      ragautas: false,
+      ingredientai: ['žalia žuvis', 'daržovės', 'žalia citrina'],
+      kaina: {
+        nuo: 10,
+        iki: 20
+      }
+    }
+  ]);
+
   return (
     <>
 
