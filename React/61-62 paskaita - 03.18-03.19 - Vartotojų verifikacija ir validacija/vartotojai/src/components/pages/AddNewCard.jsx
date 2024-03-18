318f20845244ae32351e3b6f7351c026d38e2e4e
BanRokas <rokas.banaitis@gmail.com>
2024-03-18 15:33:17 +0200

    61 paskaita - Add New Card

--- React/61-62 paskaita - 03.18-03.19 - Vartotojų verifikacija ir validacija/vartotojai/src/components/pages/AddNewCard.jsx
+++ React/61-62 paskaita - 03.18-03.19 - Vartotojų verifikacija ir validacija/vartotojai/src/components/pages/AddNewCard.jsx
@@ -6,12 +6,44 @@ import * as Yup from 'yup';
 import { useNavigate } from "react-router-dom";
 import { v4 as uuid } from 'uuid';
 import styled from "styled-components";
+import { CardsActionTypes } from "../../contexts/CardsContext";
 
 const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 70px;
 
+  > h1{
+    font-size: 3rem;
+  }
+
+  > form{
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+
+    > div {
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+
+      > textarea{
+        height: 5lh;
+      }
+      > p{
+        grid-column: span 3;
+        color: red;
+        text-align: center;
+      }
+    }
+  }
 `;
 
 const AddNewCard = () => {
+
+  const navigate = useNavigate();
+  const { loggedInUser } = useContext(UsersContext);
+  const { setCards } = useContext(CardsContext);
 
   const formik = useFormik({
     initialValues: {
@@ -19,7 +51,17 @@ const AddNewCard = () => {
       description: ""
     }, 
     onSubmit: values => {
-      console.log(values);
+      const newCard = {
+        id: uuid(),
+        userId: loggedInUser.id,
+        ...values
+      }
+      // console.log(newCard);
+      setCards({
+        type: CardsActionTypes.addNew,
+        data: newCard
+      });
+      navigate(-1);
     },
     validationSchema: Yup.object({
       title: Yup.string()
